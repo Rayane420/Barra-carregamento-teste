3b54157e61b013b95fc6f3828f5c6b4aeef9c1fe
Rayane Ribeiro <46855211+Rayane420@users.noreply.github.com>
2020-08-21 23:20:15 -0300

    Adicionando novo projeto
    Projeto de teste com JSP e Servlets Avançado com JSON + Ajax + Gráficos + Relatórios e Autenticação

---
+++ javaEstudo/src/filter/FilterAutenticacao.java
@@ -0,0 +1,58 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import user.UserLogado;
+
+@WebFilter(urlPatterns = {"/pages/*"})
+public class FilterAutenticacao implements Filter{
+
+	//Faz alguma coisa quando a aplicação é derrubada
+	@Override
+	public void destroy() {
+		Filter.super.destroy();
+	}
+	
+	//intercepta todas as requisições
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		//capturando se o usuário está logado
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		String urlParaAutenticar = req.getServletPath();
+		
+		//Retorna null caso não esteja logado
+		UserLogado userLogado = (UserLogado) session.getAttribute("usuario");
+		
+		if(userLogado == null && !urlParaAutenticar.equalsIgnoreCase("/pages/ServletAutenticacao")) {  //usuário não logado
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/autenticar.jsp?url="+urlParaAutenticar);
+			dispatcher.forward(request, response);
+			return;
+		}
+		
+		//executa ações do request e response
+		chain.doFilter(request, response);
+		
+	}
+	
+	//Faz alguma coisa quando a aplicação é iniciada
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+
+}
